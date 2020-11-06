1b543bc7661db1166e7b4c1559717b787ba479eb
Dennis Boeckmann <dev@dbck.de>
2020-11-06 21:15:26 +0100

    Fixed some issues

--- src/test/java/de/dbck/poc/pocjunit/HelloWorldTest.java
+++ src/test/java/de/dbck/poc/pocjunit/HelloWorldTest.java
@@ -1,7 +1,6 @@
 package de.dbck.poc.pocjunit;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
--- src/test/java/de/dbck/poc/pocjunit/SimpleTestMockIT.java
+++ src/test/java/de/dbck/poc/pocjunit/SimpleTestMockIT.java
@@ -5,14 +5,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 @Tag("integration")
 class SimpleTestMockIT {
 
   @Test
   void oneIsOneMockIT() throws Exception {
-    assertThat(1).isEqualTo(1);
+    assertThat(Integer.parseInt("1")).isEqualTo(1);
   }
 }
